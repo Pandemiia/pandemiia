d8d3d13114e79eb4ae00b44c38bbe4e2829dcb67
Yurii Andrusiak <yurii.andrusiak@gmail.com>
2020-03-24 11:42:56 +0200

    Rename folder to pandemiia

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'perekazy.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pandemiia.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
