3c0cc1106311f456beb435ed508f359864f5b8e0
Korzhak <navchalkin@ex.ua>
2020-03-30 12:08:55 +0300

    added api docs

--- pandemiia/settings/dev.py
+++ pandemiia/settings/dev.py
@@ -12,7 +12,7 @@ DATABASES = {
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, '../static'),
+    '/home/bob/Programming/pandemiia/static',
 )
 
 
