4936955b52773aa69abbd6df7d8e7bddd33c7cde
Valerii Pokrytiuk <pokrytiuk.valerii@steelkiwi.com>
2020-04-15 16:47:17 +0300

    update wsgi

--- pandemiia/wsgi.py
+++ pandemiia/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pandemiia.settings.local_settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pandemiia.settings')
 
 application = get_wsgi_application()
