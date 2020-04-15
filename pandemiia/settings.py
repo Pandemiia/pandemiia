d7d9b283a75e7f9c17ddb402c5f7aaed6377005d
Valerii Pokrytiuk <pokrytiuk.valerii@steelkiwi.com>
2020-04-15 15:19:08 +0300

    Reconfigure settings

---
+++ pandemiia/settings.py
@@ -0,0 +1,148 @@
+import os
+import environ
+
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = environ.Path(__file__) - 2  # (/a/myfile.py - 2 = /)
+
+env = environ.Env(
+    DJANGO_DEBUG=(bool, False),
+    DJANGO_SECRET_KEY=(str, 'CHANGEME!!!j&3a)=&eflya=2@hmg!tsa(mb6h3lcno4#p9oct$o1&@l75-%2',),
+    DJANGO_ADMINS=(list, []),
+    DJANGO_ALLOWED_HOSTS=(list, []),
+    DJANGO_STATIC_ROOT=(str, str(BASE_DIR('staticfiles'))),
+    DJANGO_MEDIA_ROOT=(str, str(BASE_DIR('media'))),
+    DJANGO_DATABASE_URL=(str, 'sqlite:///pandemiia'),
+    DJANGO_EMAIL_URL=(environ.Env.email_url_config, 'consolemail://'),
+    DJANGO_DEFAULT_FROM_EMAIL=(str, 'admin@example.com'),
+    DJANGO_EMAIL_BACKEND=(str, 'django.core.mail.backends.smtp.EmailBackend'),
+    DJANGO_SERVER_EMAIL=(str, 'root@localhost.com'),
+    DJANGO_CELERY_BROKER_URL=(str, 'redis://localhost:6379/0'),
+    DJANGO_CELERY_BACKEND=(str, 'redis://localhost:6379/0'),
+    DJANGO_CELERY_ALWAYS_EAGER=(bool, False),
+    DJANGO_USE_DEBUG_TOOLBAR=(bool, False),
+    DJANGO_TEST_RUN=(bool, False),
+    DJANGO_HEALTH_CHECK_BODY=(str, 'Success'),
+)
+environ.Env.read_env()
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = env.bool('DJANGO_DEBUG')
+
+SECRET_KEY = env('DJANGO_SECRET_KEY')
+
+ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS')
+
+ADMINS = tuple(
+    [tuple(admins.split(':')) for admins in env.list('DJANGO_ADMINS')],
+)
+
+DATABASES = {
+    'default': env.db('DJANGO_DATABASE_URL'),
+}
+
+DEFAULT_FROM_EMAIL = env('DJANGO_DEFAULT_FROM_EMAIL')
+
+SERVER_EMAIL = env('DJANGO_SERVER_EMAIL')
+
+# SECURE_SSL_REDIRECT = False®
+# CSRF_COOKIE_SECURE = True
+# SESSION_COOKIE_SECURE = True
+
+# Application definition
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'medsupport',
+    'rest_framework',
+    'corsheaders'
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+]
+
+ROOT_URLCONF = 'pandemiia.urls'
+
+CORS_ORIGIN_ALLOW_ALL = True
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [str(BASE_DIR.path('templates')),],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.template.context_processors.i18n',
+                'django.template.context_processors.media',
+                'django.template.context_processors.static',
+                'django.template.context_processors.tz',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'pandemiia.wsgi.application'
+
+STATIC_URL = '/static/'
+STATIC_ROOT = env('DJANGO_STATIC_ROOT')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = env('DJANGO_MEDIA_ROOT')
+
+
+# Password validation
+# https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+
+# Internationalization
+# https://docs.djangoproject.com/en/2.2/topics/i18n/
+LANGUAGE_CODE = 'uk'
+
+TIME_ZONE = 'EET'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+REST_FRAMEWORK = {
+    # 'DEFAULT_FILTER_BACKENDS': (
+    #     'django_filters.rest_framework.DjangoFilterBackend',
+    # ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 1000,
+}
+
