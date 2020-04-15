aa01cc2198db9d742e226032b75d18e3ccf6f5ed
Valerii Pokrytiuk <pokrytiuk.valerii@steelkiwi.com>
2020-04-15 18:18:08 +0300

    Update drf settings

--- pandemiia/settings.py
+++ pandemiia/settings.py
@@ -151,6 +151,6 @@ REST_FRAMEWORK = {
     #     'django_filters.rest_framework.DjangoFilterBackend',
     # ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 1000,
+    'PAGE_SIZE': 100,
 }
 
