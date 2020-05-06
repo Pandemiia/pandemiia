ba6632aa85d5965ad2e83de70020fbe3e3744458
Valerii Pokrytiuk <pokrytiuk.valerii@steelkiwi.com>
2020-05-06 14:35:39 +0300

    Add email settings

--- pandemiia/settings.py
+++ pandemiia/settings.py
@@ -42,11 +42,23 @@ DATABASES = {
     'default': env.db('DJANGO_DATABASE_URL'),
 }
 
-# Email configs
-EMAIL_BACKEND = env('DJANGO_EMAIL_BACKEND')
+# Django Email Server
+# https://docs.djangoproject.com/en/2.2/ref/settings/#email-backend
+# ------------------------------------------------------------------------------
+EMAIL_URL = env.email_url('DJANGO_EMAIL_URL')
+EMAIL_BACKEND = EMAIL_URL['EMAIL_BACKEND']
+EMAIL_HOST = EMAIL_URL.get('EMAIL_HOST', '')
+EMAIL_HOST_PASSWORD = EMAIL_URL.get('EMAIL_HOST_PASSWORD', '')
+EMAIL_HOST_USER = EMAIL_URL.get('EMAIL_HOST_USER', '')
+EMAIL_PORT = EMAIL_URL.get('EMAIL_PORT', '')
+EMAIL_USE_SSL = 'EMAIL_USE_SSL' in EMAIL_URL
+EMAIL_USE_TLS = 'EMAIL_USE_TLS' in EMAIL_URL
+EMAIL_FILE_PATH = EMAIL_URL.get('EMAIL_FILE_PATH', '')
+EMAIL_SUBJECT_PREFIX = ''
 DEFAULT_FROM_EMAIL = env('DJANGO_DEFAULT_FROM_EMAIL')
 SERVER_EMAIL = env('DJANGO_SERVER_EMAIL')
-SITE_ID = env('DJANGO_SITE_ID')
+
+
 # SECURE_SSL_REDIRECT = False
 # CSRF_COOKIE_SECURE = True
 # SESSION_COOKIE_SECURE = True
