5ee40aa32a38e3ce92e5572f55365457525edd79
Volodymyr Bilyi <bilyi.volodymyr@steelkiwi.com>
2020-05-06 17:24:57 +0300

    updated sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,5 +2,5 @@
 python manage.py migrate       
 python manage.py collectstatic --noinput
 /usr/sbin/nginx
-exec gunicorn config.wsgi --bind=0.0.0.0:8000
+exec gunicorn pandemiia.wsgi --bind=0.0.0.0:8000
 
